a8420be041f5687a4614b5da95c39bddc559c405
Алексей Орел <orel333@mail.ru>
2024-06-05 20:44:22 +0300

    geo recognizing

--- streets_backend/utils/methods.py
+++ streets_backend/utils/methods.py
@@ -1,4 +1,9 @@
+import os
+
+import requests
+
 from random import randint
+from typing import Optional
 
 from aboutus.models import Region
 
@@ -8,6 +13,27 @@ def get_confirmation_code():
     return randint(100000000, 999999999)
 
 
-def get_regions_from_ip():
+def get_region_from_ip(ip: str) -> list[Region]:
     """Получение релевантных регионов из ip."""
-    pass
+    region_name = get_location(ip)
+    print(region_name)
+    if region_name is None:
+        return None
+    region_object = Region.objects.get(name=region_name)
+    return region_object.id
+
+
+def get_location(ip: str) -> Optional[str]:
+    """Получение информации о геопозиции."""
+    response = requests.get(f"http://ip-api.com/json/{ip}?lang=ru")
+    if response.status_code == 404:
+        return None
+    result = response.json()
+    if result["status"] == "fail":
+        return None
+    country = result.get('country')
+    if country != 'Россия':
+        return None
+    region = result.get('regionName')
+    return region
+    
